21526f568dd16521a922b7227f97df330d1e701a
乐吴 <2471236749@qq.com>
2021-08-27 08:48:32 +0800

    上传基线代码

---
+++ MFCChatServer/resource.h
@@ -0,0 +1,34 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCChatServer.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCCHATSERVER_DIALOG        102
+#define IDR_MAINFRAME                   128
+#define IDC_START                       1000
+#define IDC_DISCONNECT_Ser              1001
+#define IDC_CALC                        1002
+#define IDC_MAIL                        1003
+#define IDC_QQ                          1004
+#define IDC_CLEAR_Ser                   1005
+#define IDC_SEND_Ser                    1006
+#define IDC_HIS_LIST                    1007
+#define IDC_EDIT2                       1012
+#define IDC_EDIT3                       1013
+#define IDC_MASSGE_Ser                  1013
+#define IDC_COLOR_Ser                   1014
+#define ID_HISTORY_Ser                  1015
+#define ID_TEXTCOLOR_Ser                1016
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1017
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
---
+++ MFCchatClinet/resource.h
@@ -0,0 +1,35 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCchatClinet.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCCHATCLINET_DIALOG        102
+#define IDR_MAINFRAME                   128
+#define IDC_CONNECTION                  1000
+#define IDC_DISCONNECT                  1001
+#define IDC_SEND                        1002
+#define IDC_DELETE                      1003
+#define IDC_EDIT2                       1007
+#define IDC_EDIT3                       1008
+#define IDC_COMBO1                      1012
+#define IDC_LIST4                       1013
+#define ID_HISTORY                      1021
+#define IDC_TEXTCOLOR                   1022
+#define IDC_EDIT5                       1026
+#define IDC_IPADDRESS1                  1027
+#define IDC_PORT                        1028
+#define IDC_AUTO                        1029
+#define IDC_AUTO_REPLY                  1029
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1030
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
